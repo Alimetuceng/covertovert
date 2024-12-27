8648f68a0a910279a39e4502640d73edf8f7f295
priest <mrkilinc22@gmail.com>
2024-12-27 13:27:35 +0300

    Basic functioning version. Copied from Phase 1

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -1,25 +1,29 @@
 from CovertChannelBase import CovertChannelBase
+from scapy.all import IP, ICMP, sniff
+
 
 class MyCovertChannel(CovertChannelBase):
-    """
-    - You are not allowed to change the file name and class name.
-    - You can edit the class in any way you want (e.g. adding helper functions); however, there must be a "send" and a "receive" function, the covert channel will be triggered by calling these functions.
-    """
-    def __init__(self):
-        """
-        - You can edit __init__.
-        """
-        pass
-    def send(self, log_file_name, parameter1, parameter2):
-        """
-        - In this function, you expected to create a random message (using function/s in CovertChannelBase), and send it to the receiver container. Entire sending operations should be handled in this function.
-        - After the implementation, please rewrite this comment part to explain your code basically.
-        """
-        binary_message = self.generate_random_binary_message_with_logging(log_file_name)
-        
-    def receive(self, parameter1, parameter2, parameter3, log_file_name):
-        """
-        - In this function, you are expected to receive and decode the transferred message. Because there are many types of covert channels, the receiver implementation depends on the chosen covert channel type, and you may not need to use the functions in CovertChannelBase.
-        - After the implementation, please rewrite this comment part to explain your code basically.
-        """
-        self.log_message("", log_file_name)
+    def send(self, **kwargs):
+        # Destination hostname or IP
+        dest = "receiver"
+
+        # Define the IP and ICMP layers
+        ip_layer = IP(dst=dest, ttl=1)
+        icmp_layer = ICMP()
+
+        # Send the packet
+        print(f"Sending packet to {dest}...")
+        super().send(ip_layer / icmp_layer)
+        print("Packet sent successfully!")
+
+    def receive(self, **kwargs):
+        # Function to process received packets
+        def process_packet(pckt):
+            if pckt.haslayer(ICMP) and pckt[IP].ttl == 1:
+                print("Packet received!")
+                pckt.show()
+
+        # Start sniffing for ICMP packets
+        print("Waiting for packets...")
+        sniff(filter="icmp", prn=process_packet, timeout=10)
+               
