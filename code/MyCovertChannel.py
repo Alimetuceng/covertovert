d82a278052b8c63574538174efa41d86d6735b45
priest <mrkilinc22@gmail.com>
2024-12-27 14:54:33 +0300

    Complete messages formed.

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -7,25 +7,29 @@ class MyCovertChannel(CovertChannelBase):
 
     def send(self, **kwargs):
 
+
+        sent_messages = []
         
         for i in range(5):
 
             message = self.generate_random_message( min_length=50, max_length=100)
+            sent_messages.append(message)
+            
             payload = Raw(load=message)
-
+            
 
             ip_layer = IP(dst="receiver", ttl=1)
             super().send(ip_layer / payload)
 
 
-            log_file_name = kwargs.get("log_file_name")
-            self.log_message(message=message, log_file_name=log_file_name)
-
-
             print(f"Message {i + 1} sent: {message}\n")
 
-
-
+        
+        log_file_name = kwargs.get("log_file_name")
+        merged_messages = "".join(sent_messages)
+        self.log_message(message=merged_messages, log_file_name=log_file_name)
+        
+        
         print("Raw packets are sent successfully!\n")
 
 
@@ -40,22 +44,24 @@ class MyCovertChannel(CovertChannelBase):
             if pckt.haslayer(IP) and pckt[IP].ttl == 1 and pckt.haslayer(Raw):
                     
 
-                    payload = pckt[Raw].load.decode("utf-8", errors="ignore")
+                    payload_message = pckt[Raw].load.decode("utf-8", errors="ignore")
                     
 
-                    received_messages.append(payload)
-                    print(f"Payload: {payload} is added to received_messages.\n")
-                    
-
-                    log_file_name = kwargs.get("log_file_name")
-                    self.log_message(message=payload, log_file_name=log_file_name)
+                    received_messages.append(payload_message)
+                    print(f"Payload: {payload_message} is added to received_messages.\n")
 
         
         print("Waiting for raw IP packets...\n")
 
 
-        sniff(filter="ip", prn=process_packet, timeout=30)
-        
+        sniff(filter="ip", prn=process_packet, timeout=10)
+
+
+        log_file_name = kwargs.get("log_file_name")
+
+
+        merged_messages = "".join(received_messages)
+        self.log_message(message=merged_messages, log_file_name=log_file_name)
         
         
         
