2670ac86c8465137cd9f5e35b0ee9e12dce8c156
priest <mrkilinc22@gmail.com>
2024-12-27 13:55:19 +0300

    Logs are added. Receiver might need a change

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -5,15 +5,23 @@ from scapy.all import IP, Raw, sniff, send
 class MyCovertChannel(CovertChannelBase):
     def send(self, **kwargs):
 
-        ip_layer = IP(dst="receiver", ttl=1)
 
-        # Add an optional payload (Raw layer)
-        message = "  Raw packet payload  "
+        message = "  Raw packet payload 2.0 extra platinium etc"
         payload = Raw(load=message)
 
+
+        ip_layer = IP(dst="receiver", ttl=1)
         super().send(ip_layer / payload)
 
+
+        log_file_name = kwargs.get("log_file_name")
+        self.log_message(message=message, log_file_name=log_file_name)
+
+
+
         print("Raw IP packet sent successfully!")
+
+
 
     def receive(self, **kwargs):
 
@@ -23,7 +31,12 @@ class MyCovertChannel(CovertChannelBase):
                 print("Raw IP packet received!")
                 if pckt.haslayer(Raw):
                     print(f"Payload: {pckt[Raw].load}")
+                    log_file_name = kwargs.get("log_file_name")
+                    self.log_message(message="payloadContentHere", log_file_name=log_file_name)
+
 
         # Start sniffing for IP packets
         print("Waiting for raw IP packets...")
         sniff(filter="ip", prn=process_packet, timeout=10)
+
+        
