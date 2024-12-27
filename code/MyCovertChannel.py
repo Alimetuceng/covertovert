d0b7902c974138253fbcca38e3733ecd0f0e046f
priest <mrkilinc22@gmail.com>
2024-12-27 14:43:24 +0300

    Multiple messages sent . Still contant. Logging only the last message.

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -3,44 +3,59 @@ from scapy.all import IP, Raw, sniff
 
 
 class MyCovertChannel(CovertChannelBase):
+
+
     def send(self, **kwargs):
 
-        message = self.generate_random_message( min_length=50, max_length=100)
-        payload = Raw(load=message)
+        
+        for i in range(5):
+
+            message = self.generate_random_message( min_length=50, max_length=100)
+            payload = Raw(load=message)
 
 
-        ip_layer = IP(dst="receiver", ttl=1)
-        super().send(ip_layer / payload)
+            ip_layer = IP(dst="receiver", ttl=1)
+            super().send(ip_layer / payload)
 
 
-        log_file_name = kwargs.get("log_file_name")
-        self.log_message(message=message, log_file_name=log_file_name)
+            log_file_name = kwargs.get("log_file_name")
+            self.log_message(message=message, log_file_name=log_file_name)
+
+
+            print(f"Message {i + 1} sent: {message}\n")
 
 
 
-        print("Raw IP packet sent successfully!")
+        print("Raw packets are sent successfully!\n")
 
 
 
     def receive(self, **kwargs):
 
-        def process_packet(pckt):
-            if pckt.haslayer(IP) and pckt[IP].ttl == 1:
-                if pckt.haslayer(Raw):
+        received_messages = []
 
+
+        def process_packet(pckt):
+
+            if pckt.haslayer(IP) and pckt[IP].ttl == 1 and pckt.haslayer(Raw):
+                    
 
                     payload = pckt[Raw].load.decode("utf-8", errors="ignore")
-                    print(f"Payload: {payload}")
                     
+
+                    received_messages.append(payload)
+                    print(f"Payload: {payload} is added to received_messages.\n")
                     
+
                     log_file_name = kwargs.get("log_file_name")
                     self.log_message(message=payload, log_file_name=log_file_name)
 
-
         
-        print("Waiting for raw IP packets...")
+        print("Waiting for raw IP packets...\n")
 
 
-        sniff(filter="ip", prn=process_packet, timeout=10)
-
+        sniff(filter="ip", prn=process_packet, timeout=30)
+        
+        
+        
         
