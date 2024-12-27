ae584b2ba071294392555c9f3b98897b15f61aa9
priest <mrkilinc22@gmail.com>
2024-12-27 22:28:26 +0300

    Bit level packages work. Terminate condition for sniff is missing . Bits must be determined based on package size rather than message content.

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -4,82 +4,68 @@ from scapy.all import IP, Raw, sniff
 
 class MyCovertChannel(CovertChannelBase):
 
-    def decode_message(self, input_message, chunk_size=20):
-        """
-        Encodes the input message by splitting it into chunks.
-        """
-        return [input_message[i:i + chunk_size] for i in range(0, len(input_message), chunk_size)]
-
-
-    def encode_message(self, input_message, chunk_size=20):
-        message_chunks = []
-
-        for i in range(0, len(input_message), chunk_size):
-            chunk = input_message[i:i + chunk_size] 
-            message_chunks.append(chunk)
-        
-        return message_chunks
-
-
-
-
-
     def send(self, **kwargs):
-
-        log_file_name = kwargs.get("log_file_name")
-        input_message = self.generate_random_binary_message_with_logging( log_file_name=log_file_name, min_length=50, max_length=100)
         
+        # Generate a random input message for current implementation
+        input_message = self.generate_random_message( min_length=50, max_length=100 )
+
+
+        # Get log file      
+        log_file_name = kwargs.get("log_file_name", "sender.log")
+        
+        # Log input file
+        self.log_message( message=input_message, log_file_name=log_file_name )
+
+        # Convert input message to binary
+        binary_message = self.convert_string_message_to_binary(input_message)
+
+        print(f"Input Binary Message: {binary_message}\n")
+
 
         index = 1
-        for chunk in input_message:
 
-            if(chunk == '0'):
-                message = '0'
-            else:
-                message = '1'
+        for bit in binary_message:
+
+            payload = Raw(load=bit)
             
-            payload = Raw(load=message)
             
             ip_layer = IP(dst="receiver", ttl=1)
             super().send(ip_layer / payload)
 
-            print(f"Message Chunk {index} sent: {chunk}\n")
-            index += 1
+            
 
+            print(f"Bit {index} sent: {bit}\n")
+            index += 1
 
 
 
     def receive(self, **kwargs):
 
-        received_messages = []
+        log_file_name = kwargs.get("log_file_name", "receiver.log")
 
+        received_bits = []
+        decoded_message = []
 
         def process_packet(pckt):
+            nonlocal received_bits, decoded_message
 
             if pckt.haslayer(IP) and pckt[IP].ttl == 1 and pckt.haslayer(Raw):
+
+                bit = pckt[Raw].load.decode("utf-8", errors="ignore")
+                received_bits.append(bit)
+
+
+                if len(received_bits) == 8:
+                    
+                    char = self.convert_eight_bits_to_character("".join(received_bits))
+                    decoded_message.append(char)
+
+                    received_bits = []
+
+                    print(f"Converted 8 bits to character: {char}\n")
                     
 
-                    payload_message = pckt[Raw].load.decode("utf-8", errors="ignore")
-                    
+        sniff(filter="ip", prn=process_packet, timeout=20)
 
-                    received_messages.append(payload_message)
-                    
-                    
-                    
-                    print(f"Payload: {payload_message} is added to received_messages.\n")
-
-        
-        print("Waiting for raw IP packets...\n")
-
-
-        sniff(filter="ip", prn=process_packet, timeout=10)
-
-
-        log_file_name = kwargs.get("log_file_name")
-
-
-        merged_messages = "".join(received_messages)
-        self.log_message(message=merged_messages, log_file_name=log_file_name)
-        
-        
-        
+        merged_message = "".join(decoded_message)
+        self.log_message(message=merged_message, log_file_name=log_file_name)
